800a8822371bbe455bceb89afe2ca4bbbd325f2a
yurkor.83 <yurkor.83@276842fb-754e-1281-c138-eafd5ce9f59c>
2011-02-04 15:34:36 +0000

    initial commit

---
+++ src/test/java/com/myml/gexp/examples/ExamplesTest.java
@@ -0,0 +1,17 @@
+package com.myml.gexp.examples;
+
+/**
+ * Author: Yura Korolov
+ * Date: 04.02.2011
+ * Time: 17:14:51
+ */
+public class ExamplesTest {
+
+    public void sudukuSolver() {
+
+    }
+
+    public void personWithJobTitle() {
+
+    }
+}
