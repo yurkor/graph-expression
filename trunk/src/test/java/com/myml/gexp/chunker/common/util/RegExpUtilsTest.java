7423125bae6de55478a56e8eb9b9ab09709aff97
yurkor.83 <yurkor.83@276842fb-754e-1281-c138-eafd5ce9f59c>
2011-03-12 13:47:06 +0000

    some code

---
+++ trunk/src/test/java/com/myml/gexp/chunker/common/util/RegExpUtilsTest.java
@@ -0,0 +1,21 @@
+package com.myml.gexp.chunker.common.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Author: Yura Korolov
+ * Date: 12.03.11
+ * Time: 15:37
+ */
+public class RegExpUtilsTest {
+        @Test
+        public void test() {
+                String[] examples = new String[]{"javvva", "javggaaa", "javajava", "adsasd", "adasddsa"};
+                String optimizedRegexp = RegExpUtils.convertListToRegexp(true, examples);
+                Assert.assertEquals("(?:ad(?:asddsa|sasd)|jav(?:ajava|ggaaa|vva))", optimizedRegexp);
+                for(String s : examples) Assert.assertTrue(s.matches(optimizedRegexp));
+        }
+}
